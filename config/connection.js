0294217968c6dce89f913f0c771ea8ccc78c58cb
Melinda Hardin <melindawad@gmail.com>
2018-05-01 20:44:09 -0500

    her

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,28 @@
 //code to connect Node to MySQL. Export the connection.
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     // hide password
     password: "#",
     database: "burgers_db"
+  });
+};
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
 });
 
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
-
 module.exports = connection;
+
